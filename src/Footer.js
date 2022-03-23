ad47f669ed69424da913585a62039685cf5a0d6b
Max <mnosov622@gmail.com>
2022-03-23 18:03:17 +0700

    about=me-card change

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './Footer.css'
 
-import vkLink from './images/vk-link.png';
-
-import telegram from './images/telegram-link.png';
 import social from './images/social-links.png';
 
 export default function Footer(){
--- src/Header.js
+++ src/Header.js
@@ -6,19 +6,20 @@ import useMediaQuery from "./hooks/useMediaQuery"
 import button from './images/button1.png';
 import './fonts/Gilroy-ExtraBold.otf';
 
-function Header() {
+export default function Header() {
+  const isDesktop = useMediaQuery('(min-width: 860px)');
+
   const noInfo=()=>{
     alert('Здесь пока ничего нет...');
   }
-  const isDesktop = useMediaQuery('(min-width: 860px)');
-
+  
   return (
     <body>
 
-      <div class="container">
-    <div class="header">
+       <div class="container">
+       <div class="header">
 
-      <ul>
+       <ul>
         
         <li>
           <img class="mypic" src={mypic}></img>
@@ -27,38 +28,33 @@ function Header() {
         
         </li>
         
-        <a href='http://localhost:3000'>
+      <a href='http://localhost:3000'>
       <li class="ilink">
-      
       <img class="main-logo" src={logo}></img>
       </li>
       </a>
 
 
-    <li>
+      <li>
      
       {isDesktop ? 
       
       <a href="#"><button onClick={noInfo} class="panel"><p class="panel-text">Панель управления</p></button> </a>
-    :  
-    <a href="#">
-    <img  class="mobile-button" src={button}></img>
-    </a>
-    }
+      :  
+      <a href="#">
+      <img  class="mobile-button" src={button}></img>
+      </a>
+
+      }
  
-    </li>
+      </li>
       </ul>
 
-    </div> 
-
-    
-
-    
-    </div>
+     </div> 
+     </div>
 
 
     </body>
   )
 }
 
-export default Header;
